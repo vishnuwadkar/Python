c06ebb0b5fedfb4dfb15bf02b4abca5776e7d46c
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-09-23 00:02:44 +0530

    Python

---
+++ vid1.py
@@ -0,0 +1,45 @@
+print("Hello World!")
+#prints hello world!
+#here print is a function
+
+#Variables in python!
+#Variable are names given to memory location in a program
+name = "Vishnu"     #here 'name' is  a variable
+age = 20
+
+print(name)     #printing using variable name
+print(age)
+
+print("My name is: ", name, "and my age is ",age)     #printing using variable name with string
+
+# assigning values to variables
+age2 = age
+#thus age's value is assigned to age2 variable
+
+#type fucntion: returns type of a variable
+print(type(age))    #int
+print(type(name))   #str(string)
+
+#printing sum using variables
+a=2
+b=4
+sum = a+b
+print(sum)
+
+#assignment operators in python
+num = 10
+num = num + 2
+#now value of num becomes 12 i.e 10 + 2
+
+#this can be simplified using special operator: +=
+num1 = 10
+num1+=2
+print(num1)
+
+#similarly we can also use  -=, *=, /=, %=, **=, //=, <<=, >>=, &=, ^=
+
+
+
+
+
+
