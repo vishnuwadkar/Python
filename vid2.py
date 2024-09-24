894cc139d5769150a3384192f113a7dda77f02a8
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-09-25 00:00:48 +0530

    Vid2 Done!

--- vid2.py
+++ vid2.py
@@ -57,3 +57,80 @@ print(word.endswith("nu"))
 str = input("Enter a string: ")
 print("The length of the string is ",len(str))
 
+#CONDITIONAL STATEMENTS
+#1. if statement
+#2. if-else statement
+#3. if-elif-else statement
+
+age = int(input("Enter your age: "))
+if(age<18):
+    print("You are a minor")
+elif(18 <= age  <= 60 ):
+    print("You are an adult")
+else:
+    print("You are a senior citizen")
+
+light = input("Enter light variable: ")
+if (light == "red"):
+    print("STOP")
+elif(light=="green"):
+    print("GO")
+else:
+    print("WAIT")
+    
+#Q. Grade Students based on their marks using If-else conditionals
+
+marks = int(input("Enter your marks: "))
+
+if(marks <= 90):
+    grade = "A"
+elif(90> marks>80):
+    grade = "B"
+elif(80 >=  marks > 70):
+    grade = "C"
+elif(70 >= marks > 60):
+    grade = "D"
+else:
+    grade = "F"
+print("Your grades are: ",grade)
+
+#NESTING  OF IF-ELSE STATEMENTS
+#writing if statement inside an if statement
+
+age = int(input("Enter your age: "))
+if(age < 18):
+    if(age > 80):
+        print("You are too old to drive!") 
+    print("You can drive!")
+else:
+    print("You are minor")
+    
+#Q. Write a program to check whether a number is even or odd using if-else statement
+number =  int(input("Enter a number: "))
+if(number%2==0):
+    print("The number is even")
+else:
+    print("Number is odd!")
+    
+#Q. Write a progra to find the greatest of three numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if(num1>num2 and num1>num3):
+    gnum=num1
+elif(num2>num1 and num2>num3):
+    gnum=num2
+elif(num3>num1 and num3>num2):
+    gnum=num3
+print("The greatest number is : ", gnum)
+
+#Q. Write a program to find if a number is a multiple of 7 or not
+num = int(input("Enter a number: "))
+if(num%7==0):
+    print("The number is a multiple of 7")
+else:
+    print("The number is not a multiple of 7")
+
+
+
