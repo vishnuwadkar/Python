354f67669b68a27e70d343382d357c15ffe0383b
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-09-27 14:00:40 +0530

    Dict and Set done

--- vid4(Dict&Set).py
+++ vid4(Dict&Set).py
@@ -80,7 +80,81 @@ print(dict)
 #Set is a collection of the unordered items. Each element in the set must be unique and immutable
 #tuple can be stored in a set, but not a dictionary and list since they are mutable
 
-collection = {1,2,3,4}
+collection = {1,2,2,3,4}
+print(collection)   #duplicate values are ignored
+print(type(collection))
+print(len(collection))   #duplicates exclude
+
+#emtpy set syntax
+e_set = set()   #since {} is syntax for empty dictionary 
+
+#methods of set
+
+# 1.add("value") -> adding elementto set
+collection.add("Hi")
+collection.add("Hi")
+print(collection)   #duplicate values are ignored
+
+#2. remove(val) -> remove values from set
+collection.remove("Hi")
+print(collection)  
+
+#Unhashable i> mutable 
+
+#3. clear() -> empties the set
+
+#4. pop() -> removes a random value from set
+'''print(collection.pop())
+print(collection.pop())
+print(collection)  '''
+
+# .union(set) -> cobines two sets and returns new
+u_set = {10, 90,"diya",2,3}
+set2 = collection.union(u_set)
+print(set2)
+
+# .intersection(set) -> returns a set of common elements
+set2 = collection.intersection(u_set)
+print(set2) #2 and 3 common
+
+#Q. Store the following word meanings in a python dictionary
+
+words = {
+    "cat":"a small animal",
+    "table": ["a piece of furniture","list of facts and figures" ]
+}
+print(words)
+
+''' Q. We are given a list of subjects for students. Assume one classroom is 
+    reqd for 1 subject, How many classroom are required  by all student'''
+
+subjects = {"Python", "java", "Cpp", "Python", "Javascript", "java", "Python", "java", "Cpp","C"}   #set of classroom
+print("No of classroom reqd would be: ", len(subjects)) #prints 5
+
+'''Q. Write a program to enter marks of 3 subjects from the user and store the in a dictionary.
+    Start with an empty and add one by one. USe subject name as key and marks as value'''
+
+marks = {}
+
+eng = int(input("Enter English marks: "))
+math = int(input("Enter math marks: "))
+history = int(input("Enter history marks: "))
+
+marks.update({"english":eng})
+marks.update({"math":math})
+marks.update({"history":history})
+
+print(marks)
+
+#Q. Figure out a way to store 9 & 9.0 as separate values in set using built in datatypes
+
+values = {
+    ("int",9),      #tuples
+    ("float",9.0)
+}
+print(values)
+#another way is to store one of the either as a string
+
 
 
 
