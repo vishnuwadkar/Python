deda44500493655cbc982d63cb51e8a7a32a010d
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-10-03 01:29:19 +0530

    File IO done

--- vid7(fileIO).py
+++ vid7(fileIO).py
@@ -50,7 +50,75 @@ os.remove("demo.txt")
 #Q. Create a file 'practice.txt' and add the following to it:
 
 with open("practice.txt","w") as myfile:
-    myfile.write("Hi Everyone\nwe are learning file I/O\nusing java.\nI like programming in Java.")
-    
+    myfile.write("Hi Everyone\nwe are learning file I/O\nusing Java.\nI like programming in Java.")
 
+#Write a function to replace Java with Python
+with open("practice.txt","r") as myfile:
+    data = myfile.read()
+
+newdata=data.replace("Java","Python")
+print(newdata)
+with open("practice.txt","w") as myfile:
+    myfile.write(newdata)
+
+#Search if 'learning' exist in the file or not
+def checkword(word):    #function that takes word as argument
+    with open("practice.txt","r") as myfile:
+        data = myfile.read()
+        if data.find(word) != -1:
+            print("Found in the file")
+        else:
+            print("Not found in the file")
+
+checkword("learning")   #function call
+
+#Write a function that checks for word and tell the line in which it is present, print -1 if not present
+def checkLines(word,filename):
+    data = True
+    line=1
+    with open(filename,"r") as myfile:
+        while data:
+            data = myfile.readline()
+            if word in data:
+                print("Present in line number ",line)
+                return
+            else:
+                line += 1
+        else:
+            print(-1)
+            return
+
+checkLines("learning","practice.txt")  #prints line 2
+checkLines("programming","practice.txt")  #prints line 4
+checkLines("xprogramming","practice.txt")  #prints line 4
+
+#Q.From a file separated by commas, print how many number are even 
+with open("prac2.txt","r") as f:
+    data = f.read()
+    print(data)
+    
+    nums = data.split(",")  #creating a list named nums splitted by ','
+    print(nums)
+    count = 0
+    for ele in nums:
+        if int(ele)%2==0:   #even number check
+            count+=1        #if even, increase count
+    else:
+        print(f"There are {count} even numbers")  #prints the  count of even numbers
+
+
+'''
+To print all numbers from a file without using split
+    num=""
+    for i in range(len(data)):
+        if data[i]==",":
+            print(int(num))
+            num=""
+        else:
+            num += data[i]
+    else:
+        print(int(num))'''
+    
+os.remove("prac2.txt")
+os.remove("practice.txt")
 
