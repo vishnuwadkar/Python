60750f8b9ff96d0a64acc74809355b99a6d0a610
vishnuwadkar <vishnuwadkar2004@gmail.com>
2024-10-04 23:34:16 +0530

    OOP-Objects and Classes

--- vid8(OOP-1).py
+++ vid8(OOP-1).py
@@ -48,3 +48,76 @@ print(myStud.name)
 print(myStud.age)
 
 print(stud2("xyz",21).name)
+
+# METHODS: methods are functions that belongs to the objects 
+
+class Boys:
+    def welcome(self):  #for every method, self is a mandatory para, even if it takes no arg
+        print("Welcome to the class, Boys!")
+    def intro(self,name,marks):
+        print(f"Hello, my name is {name} and I scored {marks} marks")
+
+boy1 = Boys()   #obj creation
+boy1.welcome()  #method call
+boy1.intro("Vishnu",78)
+
+boy2 = Boys()   #obj creation
+boy2.welcome()  #method call
+boy2.intro("Harry",80)
+
+class myStud:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    def avg(self):
+        sum=0
+        for i in self.marks:
+            sum = sum + i
+        avg = sum/len(self.marks)
+        return  avg
+
+
+    
+s1=myStud("Tony Stark",[90,56,78])
+print(s1.avg())
+
+#STATIC METHOD
+#when we don't have to use the self parameter
+#works at class level
+class myStud:
+    @staticmethod   #decorator for static method
+    def hello():    #no self para reqd
+        print("Hello, I am a static method!")
+#Prac Que: Write a program to anage the account details, credits and deposits of the account
+
+class Account:
+    def __init__(self,no):
+        self.acc_no = no
+    
+    balance = 0
+    
+    def  credit(self,amt):
+        self.balance = self.balance + amt
+        print(f"Amount credited to account {self.acc_no} successfully! Available balance: {self.balance}")
+    
+    def debit(self,amt):
+        if self.balance >= amt:
+            self.balance = self.balance - amt
+            print(f"Amount debited from account {self.acc_no} successfully! Available balance: {self.balance}")
+        else:
+            print("Insufficient balance")
+    
+    def get_account_details(self,acc_no):
+        print(f"Account Number: {self.acc_no}, Balance: {self.balance}")
+
+Vishnu = Account(1210)
+Vishnu.credit(4000)
+Vishnu.debit(200)
+Vishnu.credit(100)
+Vishnu.debit(600)
+Vishnu.debit(1000)
+Vishnu.credit(5000)
+Vishnu.get_account_details(1210)
+Vishnu.credit(100)
+Vishnu.credit(2000)
+Vishnu.debit(10000)
